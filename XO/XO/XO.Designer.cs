76c19562676cc5d05379208341fcb852d354e48c
vasiyarov-evgeniy <vasiyarov-evgeniy@mail.ru>
2017-05-27 20:08:37 +0400

    Project

---
+++ XO/XO/XO.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace XO
+{
+    partial class XO
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.StartGame = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.Clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(68, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(124, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(12, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 50);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(68, 68);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 50);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button6
+            // 
+            this.button6.Enabled = false;
+            this.button6.Location = new System.Drawing.Point(124, 68);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 50);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button7
+            // 
+            this.button7.Enabled = false;
+            this.button7.Location = new System.Drawing.Point(12, 124);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 50);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button8
+            // 
+            this.button8.Enabled = false;
+            this.button8.Location = new System.Drawing.Point(68, 124);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 50);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button_Click);
+            // 
+            // button9
+            // 
+            this.button9.Enabled = false;
+            this.button9.Location = new System.Drawing.Point(124, 124);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 50);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button_Click);
+            // 
+            // StartGame
+            // 
+            this.StartGame.Location = new System.Drawing.Point(239, 21);
+            this.StartGame.Name = "StartGame";
+            this.StartGame.Size = new System.Drawing.Size(81, 32);
+            this.StartGame.TabIndex = 9;
+            this.StartGame.Text = "Начать игру!";
+            this.StartGame.UseVisualStyleBackColor = true;
+            this.StartGame.Click += new System.EventHandler(this.StartGame_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(34, 194);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 10;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(239, 67);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(67, 17);
+            this.radioButton1.TabIndex = 11;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Крестик";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(239, 90);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 17);
+            this.radioButton2.TabIndex = 12;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Нолик";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Clear
+            // 
+            this.Clear.Location = new System.Drawing.Point(239, 146);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(81, 28);
+            this.Clear.TabIndex = 13;
+            this.Clear.Text = "Очистить";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // XO
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(355, 221);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StartGame);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "XO";
+            this.Text = "XO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button StartGame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button Clear;
+    }
+}
+
---
+++ XO/XO/XO.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class XO : Form
+    {
+        Button[] buttons;
+        public XO()
+        {
+            InitializeComponent();
+
+            buttons = new Button[]
+            {
+                button1, button2, button3, button4, button5, button6, button7, button8, button9,
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private string figureU;
+        private string figureC;
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            figureU = "X";
+            figureC = "O";
+            radioButton1.Enabled = false;
+            radioButton2.Enabled = false;
+            label1.Text = "Выбран Крестик!";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            figureU = "O";
+            figureC = "X";
+            radioButton1.Enabled = false;
+            radioButton2.Enabled = false;
+            label1.Text = "Выбран Нолик!";
+        }
+
+        private void StartGame_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Enabled == false && radioButton2.Enabled == false)
+            {
+                for (int i = 0; i < buttons.Length; ++i)
+                {
+                    buttons[i].Enabled = true;
+                    buttons[i].Text = "";
+                }
+                label1.Text = "Игра началась!";
+            } 
+            else
+            {
+                label1.Text = "Выберите фигуру!";
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < buttons.Length; ++i)
+            {
+                buttons[i].Enabled = false;
+                buttons[i].Text = "";
+            }
+            label1.Text = null;
+            radioButton1.Enabled = true;
+            radioButton2.Enabled = true;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            ((Button)sender).Text = figureU;
+            if (CheckGame())
+            {
+                label1.Text = "Вы победили!";
+                FinishGame();
+                return;
+            }
+            if (ChekEmptyButton())
+            {
+                CompProgress();
+                if (CheckGame())
+                {
+                    label1.Text = "Вы проиграли!";
+                    FinishGame();
+                }
+                else if (!ChekEmptyButton())
+                {
+                    label1.Text = "Ничья!";
+                    FinishGame();
+                }
+            }
+            else
+            {
+                label1.Text = "Ничья!";
+                FinishGame();
+            }
+        }
+
+        private bool CheckGame()
+        {
+            if (button1.Text == figureU && button2.Text == figureU && button3.Text == figureU) { return true; }
+            if (button4.Text == figureU && button5.Text == figureU && button6.Text == figureU) { return true; }
+            if (button7.Text == figureU && button8.Text == figureU && button9.Text == figureU) { return true; }
+            if (button1.Text == figureU && button4.Text == figureU && button7.Text == figureU) { return true; }
+            if (button2.Text == figureU && button5.Text == figureU && button8.Text == figureU) { return true; }
+            if (button3.Text == figureU && button6.Text == figureU && button9.Text == figureU) { return true; }
+            if (button1.Text == figureU && button5.Text == figureU && button9.Text == figureU) { return true; }
+            if (button3.Text == figureU && button5.Text == figureU && button7.Text == figureU) { return true; }
+
+            if (button1.Text == figureC && button2.Text == figureC && button3.Text == figureC) { return true; }
+            if (button4.Text == figureC && button5.Text == figureC && button6.Text == figureC) { return true; }
+            if (button7.Text == figureC && button8.Text == figureC && button9.Text == figureC) { return true; }
+            if (button1.Text == figureC && button4.Text == figureC && button7.Text == figureC) { return true; }
+            if (button2.Text == figureC && button5.Text == figureC && button8.Text == figureC) { return true; }
+            if (button3.Text == figureC && button6.Text == figureC && button9.Text == figureC) { return true; }
+            if (button1.Text == figureC && button5.Text == figureC && button9.Text == figureC) { return true; }
+            if (button3.Text == figureC && button5.Text == figureC && button7.Text == figureC) { return true; }
+            return false;
+        }
+
+        private void FinishGame()
+        {
+            for (int i = 0; i < buttons.Length; ++i)
+            {
+                buttons[i].Enabled = false;
+            }
+        }
+
+        private void CompProgress()
+        {
+
+            if (buttons[new Random().Next(0, buttons.Length)].Text == "")
+            {
+                buttons[new Random().Next(0, buttons.Length)].Text = figureC;
+                return;
+            }
+            else
+            {
+                for (int i = 0; i < buttons.Length; ++i)
+                {
+                    if (buttons[i].Text == "")
+                    {
+                        buttons[i].Text = figureC;
+                        return;
+                    }
+                }
+            }
+        }
+
+        private bool ChekEmptyButton()
+        {
+            for (int i = 0; i < buttons.Length; ++i)
+            {
+                if (buttons[i].Text == "")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+               
+        
+    }
+}
