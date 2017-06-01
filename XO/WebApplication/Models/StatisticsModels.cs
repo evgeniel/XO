24ca0afbc2dd7ec9a09521126936b3aa5e514d2c
vasiyarov-evgeniy <vasiyarov-evgeniy@mail.ru>
2017-06-02 02:26:36 +0400

    json_mvc_project

---
+++ XO/WebApplication/Models/StatisticsModels.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using XO;
+
+namespace WebApplication.Models
+{
+    public class StatisticsModels
+    {
+        public List<Statistcs> StatisticsList { get; set; }
+
+        public StatisticsModels()
+        {
+            var jsonData = File.ReadAllText(@"C:\Users\EvgenieL\Source\Repos\XO\XO\XO\bin\Debug\stats.json");
+            StatisticsList = JsonConvert.DeserializeObject<List<Statistcs>>(jsonData);
+
+        }
+    }
+}
